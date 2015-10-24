d8e21dae41697d1fcef12d99c64847840e0bcf24
Rustin Wollin <rwollin96@gmail.com>
2015-10-24 11:29:20 -0400

    setting up backgrounds/bg camera

---
+++ Assets/DisableCollider.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableCollider : MonoBehaviour
+{
+    public Transform targetTransform;
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            targetTransform.GetComponent<BoxCollider2D>().enabled = false;
+            gameObject.SetActive(false);
+        }
+    }
+}
