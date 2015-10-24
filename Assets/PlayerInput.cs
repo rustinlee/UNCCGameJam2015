c02b3da19707cc79bc71dfb2d4f6fbbc921389df
Rustin Wollin <rwollin96@gmail.com>
2015-10-23 21:42:05 -0400

    flashlight aiming + standard 2D assets

--- Assets/PlayerInput.cs
+++ Assets/PlayerInput.cs
@@ -4,30 +4,64 @@ using System.Collections;
 public class PlayerInput : MonoBehaviour {
     public float walkSpeed = 20f;
 
+    private bool facingLeft;
     private Transform upperArm;
     private Transform flashlight;
+    private Rigidbody2D rigid2D;
 
 	void Start() {
+        facingLeft = true;
         upperArm = GameObject.FindGameObjectWithTag("PivotArm").transform;
         flashlight = GameObject.FindGameObjectWithTag("Flashlight").transform;
+        rigid2D = GetComponent<Rigidbody2D>();
     }
 	
     void Update() {
-        Debug.Log(Input.mousePosition);
-        //upperArm.LookAt(Input.mousePosition);
-        //upperArm.rotation = ;
-        Vector3 vectorToTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition) - flashlight.position;
+        Vector3 vectorToTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
         Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-        upperArm.rotation = q;
+        if (vectorToTarget.x < 0) {
+            facingLeft = true;
+            //transform.localScale = new Vector3(-1f, 1f, 1f);
+            transform.rotation = Quaternion.Euler(
+                transform.rotation.eulerAngles.x,
+                -180f,
+                transform.rotation.eulerAngles.z
+            );
+        } else {
+            facingLeft = false;
+            transform.rotation = Quaternion.Euler(
+                transform.rotation.eulerAngles.x,
+                0f,
+                transform.rotation.eulerAngles.z
+            );
+            //transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+
+        upperArm.localRotation = q;
+
+        if (facingLeft) {
+            upperArm.localRotation = new Quaternion(
+                upperArm.localRotation.x,
+                upperArm.localRotation.y,
+                upperArm.localRotation.z * -1.0f,
+                upperArm.localRotation.w
+            );
+            upperArm.localScale = new Vector3(-1f, -1f, 1f);
+                
+        } else {
+            upperArm.localScale = new Vector3(1f, 1f, 1f);
+        }
+
+        Debug.Log(vectorToTarget);
         //transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime);
     }
 
     void FixedUpdate() {
         float horizInput = Input.GetAxis("Horizontal");
+
         if (horizInput != 0) {
-            //GetComponent<Rigidbody2D>().AddForce(new Vector2(horizInput * walkSpeed, 0f), ForceMode2D.Force);
-            transform.Translate(new Vector3(horizInput * Time.fixedDeltaTime * walkSpeed, 0f, 0f));
+            rigid2D.velocity = new Vector2(horizInput * walkSpeed, rigid2D.velocity.y);
         }
 	}
 }
