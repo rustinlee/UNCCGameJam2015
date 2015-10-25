fd01084db4f29d9ce40f23d024bb5266a6674cb6
Rustin Wollin <rwollin96@gmail.com>
2015-10-25 00:20:51 -0400

    bottles and shatter detection

---
+++ Assets/Shatter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shatter : MonoBehaviour
+{
+    public float shatterForce;
+    public AudioClip smallClink;
+    public AudioClip mediumClink;
+    public AudioClip shatter;
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        float mag = coll.relativeVelocity.magnitude;
+        //Debug.Log(mag);
+
+        if (mag > shatterForce) {
+            AudioSource.PlayClipAtPoint(shatter, transform.position);
+            Debug.Log("crash");
+            //shatter that shit
+        } else if (mag > shatterForce / 2) {
+            AudioSource.PlayClipAtPoint(mediumClink, transform.position);
+            Debug.Log("clank");
+        } else if (mag > 1f) {
+            AudioSource.PlayClipAtPoint(smallClink, transform.position);
+            Debug.Log("clink");
+        }
+    }
+}
