e2146a8afa7c6cf24d0864c33c4197dab41d2da1
Rustin Wollin <rwollin96@gmail.com>
2015-10-25 16:41:56 -0400

    fixed radial lights (lamps)

---
+++ Assets/RadialLightFixer.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RadialLightFixer : MonoBehaviour { 
+	void LateUpdate() {
+        transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+	}
+}
