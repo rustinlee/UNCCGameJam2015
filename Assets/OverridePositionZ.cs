b5826fa16c2b1eb35a3ac0470348ad1c2ee617e9
Rustin Wollin <rwollin96@gmail.com>
2015-11-10 21:59:21 -0500

    fixed leg pos.z / transparency problem

---
+++ Assets/OverridePositionZ.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OverridePositionZ : MonoBehaviour {
+	public List<Transform> transformList;
+	public List<float> RightZValuesList;
+	public List<float> LeftZValuesList;
+	private PlayerInput playerInput;
+
+	void Start() {
+		playerInput = GameObject.FindWithTag("Player").GetComponent<PlayerInput>();
+	}
+
+	void LateUpdate() {
+		int i = 0;
+		for (i = 0; i < transformList.Count; i++) {
+			Vector3 curPos = transformList[i].localPosition;
+			if (playerInput.isFacingLeft()) {
+				transformList[i].localPosition = new Vector3(curPos.x, curPos.y, LeftZValuesList[i]);
+			} else {
+				transformList[i].localPosition = new Vector3(curPos.x, curPos.y, RightZValuesList[i]);
+			}
+		}
+	}
+}
