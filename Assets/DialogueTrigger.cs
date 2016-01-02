28cc11c6cc780d4f6fc58075d68fb8f23b90b696
Rustin Wollin <rwollin96@gmail.com>
2016-01-02 10:19:29 -0500

    added proximity dialogue coroutine

---
+++ Assets/DialogueTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DialogueTrigger : MonoBehaviour {
+	public List<string> dialogue;
+	public float triggerDistance;
+
+	private Transform player;
+
+	void Start() {
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+
+		StartCoroutine(CheckPlayerProximity());
+	}
+
+	IEnumerator CheckPlayerProximity() {
+		while (true) {
+			if (Vector3.Distance(player.position, transform.position) < triggerDistance) {
+				Debug.Log("trigger dialogue");
+			}
+			yield return new WaitForSeconds(0.25f);
+		}
+	}
+}
